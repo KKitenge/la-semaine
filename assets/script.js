dce19697448c5527590698f4ddef198623382611
Ox Noj <geneveva@gmail.com>
2023-04-06 21:38:06 -0700

    localstorage in js tied to time/hour div id

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,6 @@
 
   //code will not run until the DOM is ready for js execution
 $(function () { 
-    
-  //This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   
   //Using JQuery select all elements on the page with the element time black
   $(".time-block").each(function() {
@@ -26,30 +19,31 @@ $(function () {
       console.log(todaysTime)
   });
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //$(".description")
-  //var userInput = $(".description").localStorage.setItem("userInput", userInput);
-   
-  // TODO: Add a listener for click events on the save button.
-    $(".saveBtn").on("click", function(event){ 
-      event.preventDefault();
-      
+    // TODO: Add a listener for click events on the save button.
+    $(".saveBtn").on("click", function(){       
       var element = $(this); // this is going to be the button clicked
       var userInput = element.siblings("textarea").val(); // value typed in by user
       console.log(userInput);
       var hour = element.parent().attr("id"); // hour-#
       console.log(hour);
-      var savedInfo = [userInput, hour];
-      localStorage.setItem("savedImfoStringify", JSON.stringify(savedInfo));
-      localStorage.setItem("userInput", savedInfo);
-      localStorage.setItem("ArrayStringify", JSON.stringify(["userInput", 1,2]));
-	    localStorage.setItem("Array", ["userInput", 1,2]);
-
+      // var savedInfo = [userInput, hour];
+      // localStorage.setItem("savedInfo", JSON.stringify(savedInfo));
+      // localStorage.setItem("userInput", savedInfo);
+      // localStorage.setItem("ArrayStringify", JSON.stringify(["userInput", 1,2]));
+	    // localStorage.setItem("Array", ["userInput", 1,2]);  
+      
+      var enteredTime = $(this).parent().attr('id');
+      var textarea = $(this).siblings('textarea').val();
+      localStorage.setItem(enteredTime, textarea);
+      $('#hour-9 textarea').val(localStorage.getItem('hour-9'));
+      $('#hour-10 textarea').val(localStorage.getItem('hour-10'));
+      $('#hour-11 textarea').val(localStorage.getItem('hour-11'));
+      $('#hour-12 textarea').val(localStorage.getItem('hour-12'));
+     
 
   });
   
+ 
 
   // TODO: Add code to display the current date in the header of the page.
     var today = dayjs().format('dddd, MMMM D, YYYY h:mm A');
